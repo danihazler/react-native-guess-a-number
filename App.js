0b041c2dc9a9403c21212c008a337e2ae79ad7d1
Daniele Pastore <danihazler@gmail.com>
2021-03-22 22:36:40 +0000

    Allow only numbers

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Header } from "./components/header";
 import { StartGameScreen } from './screens/start-game-screen';
 
--- screens/start-game-screen.js
+++ screens/start-game-screen.js
@@ -1,31 +1,49 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
+import { 
+    View, 
+    Text, 
+    Button,
+    StyleSheet,
+    TouchableWithoutFeedback,
+    Keyboard
+} from 'react-native';
 import Colors from '../constants/colors';
 import { Card } from '../components/card';
 import { Input } from '../components/input';
 
 export const StartGameScreen = () => {
+    const [enteredValue, setEnteredValeu] = useState('');
+
+    const handleOnChangeValue = (value) => {
+        const number = value.replace(/[^0-9]/g, '');
+        setEnteredValeu(number);
+    }
+
     return (
-        <View style={styles.screen}>
-            <Text style={styles.title}>Start a New Game!</Text>
-            <Card style={styles.card}>
-                <Text>Select a number</Text>
-                <Input 
-                    style={styles.input} 
-                    keyboardType="number-pad"  
-                    maxLength={2} 
-                    blurOnSubmit 
-                />
-                <View style={styles.buttons}>
-                    <View style={styles.button}>
-                        <Button title="Reset" color={Colors.secondary} onPress={() => {}} />
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <View style={styles.screen}>
+                <Text style={styles.title}>Start a New Game!</Text>
+                <Card style={styles.card}>
+                    <Text>Select a number</Text>
+                    <Input 
+                        style={styles.input} 
+                        keyboardType="number-pad"  
+                        maxLength={2} 
+                        blurOnSubmit
+                        onChangeText={handleOnChangeValue}
+                        value={enteredValue}
+                    />
+                    <View style={styles.buttons}>
+                        <View style={styles.button}>
+                            <Button title="Reset" color={Colors.secondary} onPress={() => {}} />
+                        </View>
+                        <View style={styles.button}>
+                            <Button title="Confirm" color={Colors.primary} onPress={() => {}} />
+                        </View>
                     </View>
-                    <View style={styles.button}>
-                        <Button title="Confirm" color={Colors.primary} onPress={() => {}} />
-                    </View>
-                </View>
-            </Card>
-        </View>
+                </Card>
+            </View>
+        </TouchableWithoutFeedback>
     )
 }
 
