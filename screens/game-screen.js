207f85307211e889c48b9e32f725cc425934c8b2
Daniele Pastore <danihazler@gmail.com>
2021-04-05 13:02:39 +0100

    ADD: Handle guess logic

--- screens/game-screen.js
+++ screens/game-screen.js
@@ -1,30 +1,73 @@
-import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, Text, Button, Alert, StyleSheet } from 'react-native';
 import { SelectedNumber } from '../components/selected-number';
 import { guessNumber } from '../modules/guess-number';
 
 export const GameScreen = ({ userChoice }) => {
     const[currentGuess, setCurrentGuess]= useState(guessNumber(1, 100, userChoice));
+    const currentLower = useRef(1);
+    const currentGreater = useRef(100);
+
+    const handleGuess = (direction) => {
+        if(
+            (direction === 'lower' && currentGuess < userChoice) ||
+            (direction === 'greater' && currentGuess > userChoice)
+        ) {
+            Alert.alert("Don't lie!", 'You know that this is wrong...', [
+                {text: 'Sorry!', style: 'cancel'}
+            ]);
+            return;
+        }
+        if ( direction === 'lower') {
+            currentGreater.current = currentGuess;
+        } else {
+            currentLower.current = currentGuess;
+        }
+        const nextNumberGuess = guessNumber(
+            currentLower.current, 
+            currentGreater.current, 
+            currentGuess
+        );
+
+        setCurrentGuess(nextNumberGuess);
+    }
 
     return (
         <View style={styles.screen}>
             <Text>Computer's Guess</Text>
             <SelectedNumber value={currentGuess} />
             <View style={styles.buttons} >
-                <Button title="LOWER" />
-                <Button title="GREATER" />
+                <View style={styles.button}>
+                    <Button 
+                        title="LOWER" 
+                        onPress={() => handleGuess('lower')} 
+                    />
+                </View>
+                <View style={styles.button}>
+                    <Button 
+                        title="GREATER" 
+                        onPress={() => handleGuess('greater')} 
+                    />
+                </View>
             </View>
         </View>
-    )
-
+    );
 }
 
 const styles = StyleSheet.create({
     screen: {
         alignItems: 'center',
-        marginVertical: 22
+        marginVertical: 10,
+        padding: 20
+        
     },
     buttons: {
-        flexDirection: 'row'
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+        maxWidth: '80%'
+    },
+    button: {
+        width: '45%'
     }
 })
